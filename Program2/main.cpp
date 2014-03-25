93c367e685e10b56b8aeb93aea1d9d15061cbc62
Maciej Starościak <209794@student.pwr.edu.pl>
2014-03-25 10:16:45 +0100

    Program 2

--- Program2/main.cpp
+++ Program2/main.cpp
@@ -8,30 +8,29 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    Circle circle((location){4.2,2.4},5.1);
-    Circle c1((location){6.0,4.0},6.0),c2,c3;
-c3=c1*3.0;
-c2=circle+c1;
-Circle::dilosc(4);
-printf("Circle location is %.1fx%.1f.\n", 
-    circle.get_location().x, circle.get_location().y);
-printf("Circle radius is %.1f.\n", 
-    circle.get_radius());
-printf("Circle circumference is %.2f.\n", 
-    circle.circumference());
-printf("Circle area is %.2f.\n\n", 
-    circle.area());
-    
-printf("Circle location is %.1fx%.1f.\n",
-    c2.get_location().x, circle.get_location().y);
-printf("Circle radius is %.1f.\n\n",
-    c2.get_radius());
-    
-printf("Circle location is %.1fx%.1f.\n",
-    c3.get_location().x, circle.get_location().y);
-printf("Circle radius is %.1f.\n\n",
-    c3.get_radius());
-printf("ilosc obiektow: %d\n",Circle::ilosc());
+    Circle xx[10];
+    xx[0].set_location((location){4.2,2.4});
+    xx[0].set_radius(5.2);
+    Circle::dilosc();
+    xx[1]=xx[0]*5.6;
+    Circle::dilosc();
+    for (int i=2;i<5;i++)
+    {
+        xx[i]=xx[i-1]+xx[i-2];
+        Circle::dilosc();
+    }
+
+
+    for (int i=0;i<Circle::ilosc();i++)
+    {
+        cout<<"Obiekt "<<i+1<<endl;
+        printf("Circle location is %.1fx%.1f\n",xx[i].get_location().x, xx[i].get_location().y);
+        printf("Circle radius is %.1f\n",xx[i].get_radius());
+        printf("Circle cirmference is %.2f\n",xx[i].circumference());
+        printf("Circle area is %.2f\n\n",xx[i].area());
+    }
+
+    printf("ilosc obiektow: %d\n",Circle::ilosc());
    
     system("PAUSE");
     return EXIT_SUCCESS;
